8588f6dcc432192b39fecead971eba30cfdaae43
kousaku1122 <kou1122saku@gmail.com>
2021-02-10 09:12:27 +0000

    Change insta media

--- app/controllers/api/v1/instagrams_controller.rb
+++ app/controllers/api/v1/instagrams_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::InstagramsController < ApiController
         json = Net::HTTP.get(uri)
         insta= JSON.parse(json)
         instagrams = []
-        for value in insta["data"][0..2] do
+        for value in insta["data"][0..10] do
             hash = value["id"]
             uri = URI.parse ("https://graph.instagram.com/#{hash}?fields=media_url,caption,permalink&access_token=IGQVJYZAnFJRWpkZAWRObU42R1pTaW9mbVA3NElTbzE2eDVOUnhGODBUQk1nUmJDbExSdGNHS2hnQlNRTHM3WGIwVk5tNzZAWM0FJSnpCcVlsVjhOd1prd2EyVDVRTmhmMDdvRGZAuOUd3")
             json = Net::HTTP.get(uri)
