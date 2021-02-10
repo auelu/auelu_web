d35a18aed3e2b99a310fb6e5781441d7261a5e21
kousaku1122 <kou1122saku@gmail.com>
2021-02-10 09:07:57 +0000

    Create instaAPI

--- app/javascript/packs/components/insta.vue
+++ app/javascript/packs/components/insta.vue
@@ -1,28 +1,34 @@
 <template>
      <v-container>
+       <div class="text-center h2 red--text text--darken-3" my-4>
+         Instagram投稿
+       </div>
         <v-row no-gutters class="flex-row flex-nowrap overflow-auto">
-          <v-col cols="4" v-for="list in lists" :key="list.id">
-            <v-card>{{list.value}}</v-card>
+          <v-col cols="5" md="3"v-for="list in lists" :key="list.id">
+            <v-card :href="`${list.permalink}`" width="90%">
+              <v-img :src="`${list.media_url}`" height="230px"></v-img>
+              <v-card-text class="card-text">
+                {{ list.caption }}
+              </v-card-text>
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
  data: function () {
     return {
-      lists: [
-          {id: 1, value: "aa"},
-          {id: 2, value: "aa"},
-          {id: 3, value: "aa"},
-          {id: 4, value: "aa"},
-          {id: 5, value: "aa"},
-          {id: 6, value: "aa"},
-          {id: 7, value: "aa"},
-          {id: 8, value: "aa"},
-          ]
+      lists: []
     }
+  },
+    mounted () {
+    axios
+      .get('/api/v1/instagrams.json')
+      .then(response => (this.lists = response.data))
   }
 }
 </script>
@@ -30,5 +36,11 @@ export default {
 <style scorped>
   .v-application--wrap {
     min-height: 200px !important;
+  }
+  
+  .card-text {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
   }
 </style>
