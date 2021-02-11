894f3c4f733156b713f9943dbea8edafe2980c9b
kousaku1122 <kou1122saku@gmail.com>
2021-02-11 07:41:52 +0000

    Change description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,14 @@ module ApplicationHelper
         end
     end
     
+    def document_description
+        if @description.present?
+            "#{@description}"
+        else
+            "大阪府高槻市の体育館で主に活動をしている社会人フットサルチームです。現在大阪府フットサルリーグ3部に所属しています。体験参加、練習試合相手絶賛募集中！"
+        end
+    end
+    
     def ymconv(yyyymm,cnt)
         return yyyymm + " (" + cnt + ")"
     end
