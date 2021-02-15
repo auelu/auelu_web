2c28bd39b6da178449f0f7b9c20b9a084a67f838
kousaku1122 <kou1122saku@gmail.com>
2021-02-15 20:17:52 +0000

    result

--- app/controllers/api/v2/players_controller.rb
+++ app/controllers/api/v2/players_controller.rb
@@ -10,17 +10,4 @@ class Api::V2::PlayersController < ApiController
     render json: @player
   end
 
-  private
-  def s3 
-      s3 = Aws::S3::Resource.new(
-      region: ap-northeast-1, # 1. 利用しているリージョン
-      credentials: Aws::Credentials.new(
-        AKIAWRRXXN77NI4VSEEN, # 2. プログラムからアクセスできるユーザのアクセスキー
-        yPNVZDsasYqu6EZoUmySNooyOPOLvwlTSod0CUcA # 3. プログラムからアクセスできるユーザのシークレットキー
-      )
-    )
-    signer = Aws::S3::Presigner.new(client: s3.client)
-    return presigned_url = signer.presigned_url(:get_object,
-        bucket: bucket_name, key: key, expires_in: 60)
-  end
 end
