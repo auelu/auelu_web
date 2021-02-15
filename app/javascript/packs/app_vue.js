b5a0537dcb8b0a771e6c1e965e54a8ff738b5d1f
kousaku1122 <kou1122saku@gmail.com>
2021-02-15 04:01:43 +0000

    Refact home

--- app/javascript/packs/app_vue.js
+++ app/javascript/packs/app_vue.js
@@ -3,7 +3,6 @@ import App from './app.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import router from './router.js'
-import Insta from "./components/insta.vue"
 
 Vue.use(Vuetify)
 
